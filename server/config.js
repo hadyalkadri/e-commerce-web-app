d101e922ce3cd95d6e21d20968f6c3a4f702db0e
Hady Alkadri <98332279+hadyalkadri@users.noreply.github.com>
2024-02-25 00:18:43 +0200

    Delete server/config.js

--- server/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    'secretKey': '7890-3456-5678-3241',
-    'mongoUrl': 'mongodb://hadyalkadri:mobydick1234@ac-7yrpgdx-shard-00-00.8rdblwo.mongodb.net:27017,ac-7yrpgdx-shard-00-01.8rdblwo.mongodb.net:27017,ac-7yrpgdx-shard-00-02.8rdblwo.mongodb.net:27017/?ssl=true&replicaSet=atlas-av7wvc-shard-0&authSource=admin&retryWrites=true&w=majority',
-    'mailgun_API_KEY': '6b26719a8694a263d5ff2dcd41f3e82f-cc9b2d04-6fca04f0',
-    'mailgun_DOMAIN': 'sandboxe5382637474c428086e10a5405a2f442.mailgun.org',
-    'Google_Map_API': 'AIzaSyDwPGnA1RT6cCCPHJSm0NZ_UVUwTq7Znok'
-}
-// 'mongoUrl': 'mongodb+srv://hadyalkadri:mobydick1234@ecommerce.8rdblwo.mongodb.net/?retryWrites=true&w=majority'
